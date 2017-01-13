0c39a23db70653b2dd6bc81b601caa7bd2341b25
ElenaVesna <mavakosja@gmail.com>
2017-01-13 23:54:05 +0300

    arrays tasks -updated

--- src/com/epam/elena_bogomolova/java/Main.java
+++ src/com/epam/elena_bogomolova/java/Main.java
@@ -6,10 +6,10 @@ public class Main {
         ArrayProcessing arrayActions = new ArrayProcessing();
 //        arrayActions.switchPlacesMaxMinusMinPlus();
 //        arrayActions.countEvenElements();
-//        arrayActions.replaceNegativeByZero();
-//        arrayActions.increasePositiveAfterNegative();
+//        arrayActions.replaceNegativeElementsByNumber(0);
+//        arrayActions.increasePositiveAfterNegative(3);
 //        arrayActions.differenceMinAverage();
-        arrayActions.oddRepeatingElements();
+//        arrayActions.oddRepeatingElements();
     }
 }
 
@@ -19,7 +19,7 @@ class ArrayProcessing {
         int[] myArray = new int[length];
 //        generate data
         for (int i = 0; i < myArray.length; i++) {
-            myArray[i] = (int) (Math.random()*21 - 10);
+            myArray[i] = (int) (Math.random()*(length + 1) - 10);
         }
         printArrayElements(myArray);
         return myArray;
@@ -29,15 +29,16 @@ class ArrayProcessing {
         for (int i = 0; i < myArray.length; i++) {
             System.out.println(i + " element: " + myArray[i]);
         }
+        System.out.println();
     }
 //     task 1.1
     public void switchPlacesMaxMinusMinPlus() {
         int[] myArray = new ArrayProcessing().createArray(20);
-        int maxMinus = -11;
+        int maxMinus = myArray[0];
         int positionMaxMinus = 0;
-        int minPlus = 11;
+        int minPlus = myArray[0];
         int positionMinPlus = 0;
-        for (int i = 0; i < 20; i++) {
+        for (int i = 1; i < myArray.length; i++) {
             if (myArray[i] < 0 && myArray[i] > maxMinus) {
                 maxMinus = myArray[i];
                 positionMaxMinus = i;
@@ -47,60 +48,65 @@ class ArrayProcessing {
                 positionMinPlus = i;
             }
         }
-        myArray[positionMaxMinus] = minPlus;
-        myArray[positionMinPlus] = maxMinus;
-        System.out.println("\n minimum positive and maximum negative switched places");
-        printArrayElements(myArray);
+        if (maxMinus < 0 && minPlus > 0) {
+            myArray[positionMaxMinus] = minPlus;
+            myArray[positionMinPlus] = maxMinus;
+            System.out.println("minimum positive and maximum negative switched places");
+            printArrayElements(myArray);
+        } else {
+            System.out.println("cannot switch places: no suitable elements");
+        }
+
     }
 
 //     task 1.2
     public void countEvenElements(){
         int[] myArray = new ArrayProcessing().createArray(20);
         int sumEvenElenemts = 0;
-        for (int i = 1; i < myArray.length; i = i + 2) {
-            sumEvenElenemts = sumEvenElenemts + myArray[i];
+        for (int i = 1; i < myArray.length; i += 2) {
+            sumEvenElenemts += myArray[i];
         }
-        System.out.println("\n" + "Sum of even elements: " + sumEvenElenemts);
+        System.out.println("Sum of even elements: " + sumEvenElenemts);
     }
 
 //    task 1.3
-    public void replaceNegativeByZero() {
+    public void replaceNegativeElementsByNumber(int replacingNumber) {
         int[] myArray = new ArrayProcessing().createArray(20);
         System.out.println("\n array without negative elements:");
         for (int i = 0; i < myArray.length; i++) {
             if (myArray[i] < 0) {
-                myArray[i] = 0;
+                myArray[i] = replacingNumber;
             }
         }
         printArrayElements(myArray);
     }
 
 //    task 1.4
-    public void increasePositiveAfterNegative() {
+    public void increasePositiveAfterNegative(int multiplier) {
         int[] array = new ArrayProcessing().createArray(20);
-        for (int i = 0; i < array.length; i++) {
+        for (int i = 0; i < array.length - 1; i++) {
             if (array[i] < 0 && array[i+1] > 0) {
-                array[i+1] = array[i+1]*3;
+                array[i+1] *= multiplier;
             }
         }
-        System.out.println("\n array where every positive element multiplied by 3, if it goes after the negative element");
+        System.out.println("every positive element multiplied by 3, if it goes after the negative element");
         printArrayElements(array);
     }
 
 //    task 1.5
     public void differenceMinAverage() {
         int[] array = new ArrayProcessing().createArray(20);
-        int minimum = Integer.MAX_VALUE;
+        int minimum = array[0];
         int sum = 0;
-        for (int i = 0; i < array.length; i++) {
-            sum = sum + array[i];
+        for (int i = 1; i < array.length; i++) {
+            sum += array[i];
             if (array[i] < minimum) {
                 minimum = array[i];
             }
         }
         int average = sum/array.length;
         System.out.println("minimum element in array: " + minimum + ", average: " + average);
-        System.out.println("their difference: " + (average - minimum));
+        System.out.println("their difference: " + (average - Math.abs(minimum)));
     }
 
 //    task 1.6
